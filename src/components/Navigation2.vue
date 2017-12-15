16b00d828cc0cd5ccbcd5a596f1971cd8e25b977
alexj <a.seleznev@oiler.com.ua>
2017-12-15 17:31:46 +0200

    2
    Signed-off-by: alexj <a.seleznev@oiler.com.ua>

---
+++ src/components/Navigation2.vue
@@ -0,0 +1,80 @@
+<template>
+	<div uk-sticky="media: 960" class="uk-navbar-container tm-navbar-container uk-sticky uk-sticky-fixed uk-light" >
+		<div class="uk-container uk-container-expand uk-navbar-transparent uk-light" style="position: fixed; top: 0px; ">
+
+
+			<nav class="uk-navbar">
+				<div class="uk-navbar-left">
+					<router-link class="header__logo uk-navbar-item uk-logo" to="/">
+						<img alt="ЛОГО" src="../assets/bfg-logo--white.svg" height="510" class="header__logo__img"/>
+					</router-link>
+				</div>
+				<div class="uk-navbar-right">
+					<ul class="navbar__list uk-navbar-nav">
+
+						<router-link class="nav-item" tag="li" to="/podrobnee-o-produkcii" exact>
+							<a class="navbar__list__item">О продукции</a>
+						</router-link>
+						<router-link class="nav-item" tag="li" to="/why" exact>
+							<a class="navbar__list__item">Почему?</a>
+						</router-link>
+						<router-link class="nav-item" tag="li" to="/sotrudniki" exact>
+							<a class="navbar__list__item">Наши сотрудники</a>
+						</router-link>
+						<router-link class="nav-item" tag="li" to="/faq" exact>
+							<a class="navbar__list__item">Чаво</a>
+						</router-link>
+						<router-link class="nav-item" tag="li" to="/sertificats" exact>
+							<a class="navbar__list__item">Сертификаты</a>
+						</router-link>
+						<router-link class="nav-item" tag="li" to="/gde-kupit" exact>
+							<a class="navbar__list__item">Где купить?</a>
+						</router-link>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+
+
+</template>
+
+<script>
+	export default {
+		name: 'Navigation'
+	}
+</script>
+
+<style lang="scss" scoped>
+
+	.header__logo__img {
+		height: 65px;
+		width: auto;
+		margin-left: 5px;
+	}
+
+	#navbar {
+		background: rgba(255, 111, 59, 0.95) !important;
+	}
+	/*
+		.navbar {
+			position: static;
+		}
+		.navbar__list {
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+			position: absolute;
+			right: 40vw;
+			top: 10px;
+			height: 40px;
+			line-height: 40px;
+			display: inline-flex;
+		}
+		.navbar__list__item {
+			display: inline-block;
+			position: relative;
+			margin: 0 0.6em;
+		}
+		*/
+</style>
